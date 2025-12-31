815c9b94aaf084046f6b19d5196f07d1693ffc86
Alpaca Zen <alpacazendev@gmail.com>
2025-12-30 21:47:25 -0500

    Inicio de ejercicios en .Net

---
+++ thinking-in-code/s01-basic/e01_area-triangulo.cs
@@ -0,0 +1,14 @@
+
+
+/*
+Ejemplo 1.1
+Elabore un algoritmo y su pseudocódigo para calcular e imprimir el área de un triángulo.
+*/
+
+partial class Program
+{
+    static void AreaTriangulo()
+    {
+    }   
+}
+
