e298cfb7b1a646ffd98e5bbd04bb09b8faefef74
Alpaca Zen <alpacazendev@gmail.com>
2026-01-07 05:56:46 -0500

    Lógica elemental de e02 desarrollada

--- thinking-in-code/s01-basic/e02_orden-inverso.cs
+++ thinking-in-code/s01-basic/e02_orden-inverso.cs
@@ -5,3 +5,20 @@ números enteros, escriba los mismos en orden inverso.
 */
 
 
+partial class Program
+{
+    static void OrdenInverso()
+    {
+        System.Console.Write("dato A? ");
+        string? datoA = System.Console.ReadLine();
+        System.Console.Write("dato B? ");
+        string? datoB = System.Console.ReadLine();
+        System.Console.Write("dato C? ");
+        string? datoC = System.Console.ReadLine();
+        System.Console.Write("dato D? ");
+        string? datoD = System.Console.ReadLine();
+        
+        System.Console.WriteLine($"Orden inverso: {datoD} {datoC} {datoB} {datoA}");
+    }
+}
+
