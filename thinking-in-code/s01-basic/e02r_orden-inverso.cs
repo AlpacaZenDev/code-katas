3e01c2406546f6fa3daf223ad4544bbed931aa29
Alpaca Zen <alpacazendev@gmail.com>
2026-01-08 23:01:51 -0500

    Inicio del desarrollo de e02r

--- thinking-in-code/s01-basic/e02r_orden-inverso.cs
+++ thinking-in-code/s01-basic/e02r_orden-inverso.cs
@@ -4,3 +4,18 @@ Construya un pseudocódigo, que dados los datos A, B, C y D que representan
 números enteros, escriba los mismos en orden inverso.
 */
 
+/*
+Solución altervativa con separación de responsabilidades.
+*/
+
+partial class Program
+{
+    static void OrdenInversoRev()
+    {
+        System.Console.WriteLine("\nOrden Inverso (REV)");
+        int inputA = ValidarInput("Date A? ");
+        int inputB = ValidarInput("Date A? ");
+        int inputC = ValidarInput("Date A? ");
+        
+    }
+}
