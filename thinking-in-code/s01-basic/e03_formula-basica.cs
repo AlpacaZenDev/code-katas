a6f12edd1d40ecd4a32fd78adabcddd35271fe47
Alpaca Zen <alpacazendev@gmail.com>
2026-01-10 07:11:11 -0500

    Inicio de e03. Se implementa los inputs de los datos

---
+++ thinking-in-code/s01-basic/e03_formula-basica.cs
@@ -0,0 +1,34 @@
+/*
+Construya un pseudocódigo, que dado los datos enteros A y B, escriba el resultado
+de la siguiente expresión: ((A+B)^2)/3
+
+*/
+
+
+partial class Program
+{
+    static void FormulaBasica()
+    {
+        System.Console.Write("A? ");
+        string? inputNumA = System.Console.ReadLine();
+        if (!double.TryParse(inputNumA, out double numA))
+        {
+            System.Console.WriteLine("Ingrese sólo números reales!");
+            return;
+        }
+        System.Console.Write("B? ");
+        string? inputNumB = System.Console.ReadLine();
+        if (!double.TryParse(inputNumB, out double numB))
+        {
+            System.Console.WriteLine("Ingrese sólo números reales!");
+            return;
+        }
+
+
+
+    }
+
+
+
+
+}
