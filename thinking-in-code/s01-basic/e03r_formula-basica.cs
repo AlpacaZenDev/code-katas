e1e0192dd221672457bcbf0c9b5114d538e6f381
Alpaca Zen <alpacazendev@gmail.com>
2026-01-10 22:58:45 -0500

    Avance de e03r

--- thinking-in-code/s01-basic/e03r_formula-basica.cs
+++ thinking-in-code/s01-basic/e03r_formula-basica.cs
@@ -4,9 +4,12 @@ partial class Program
 {
     static void FormulaBasicaRev()
     {
-        
+        System.Console.WriteLine("\nOperación Básica (REV)");
+        double inputNumA = ValidarInput("Num A? ");
+        double inputNumB = ValidarInput("Num B? ");
+        System.Console.WriteLine($"Resultado ");
 
-        
+
     }
 
 
