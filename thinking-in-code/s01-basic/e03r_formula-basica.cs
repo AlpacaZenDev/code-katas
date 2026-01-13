d56ab72451a187b00bb298c181fd53ad516b30b7
Alpaca Zen <alpacazendev@gmail.com>
2026-01-12 22:36:21 -0500

    avance en e03r

--- thinking-in-code/s01-basic/e03r_formula-basica.cs
+++ thinking-in-code/s01-basic/e03r_formula-basica.cs
@@ -5,11 +5,29 @@ partial class Program
     static void FormulaBasicaRev()
     {
         System.Console.WriteLine("\nOperación Básica (REV)");
-        double inputNumA = ValidarInput("Num A? ");
-        double inputNumB = ValidarInput("Num B? ");
-        System.Console.WriteLine($"Resultado ");
+        double inputNumA = ValidarInputs("Num A? ");
+        double inputNumB = ValidarInputs("Num B? ");
+        double result = CalculateOperation(inputNumA, inputNumB);
+        System.Console.WriteLine($"» Resultado = {result}");
+    }
 
+    static double ValidarInputs(string message)
+    {
+        while (true)
+        {
+            System.Console.Write(message);
+            string? input = System.Console.ReadLine();
+            if (double.TryParse(input, out double valor))
+            {
+                return valor;
+            }
+            System.Console.WriteLine("Ingrese un valor correcto!");
+        }
+    }
 
+    static double CalculateOperation(double numA, double numB)
+    {
+        return Math.Round(Math.Pow((numA + numB), 2)/3, 3);
     }
 
 
