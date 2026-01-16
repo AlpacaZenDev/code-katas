f9d9878a12ffcff94d9b4b7005c60bf7eff37279
Alpaca Zen <alpacazendev@gmail.com>
2026-01-16 05:45:42 -0500

    inicio de e04

---
+++ thinking-in-code/s01-basic/e04.cs
@@ -0,0 +1,17 @@
+
+
+
+
+partial class Program
+{
+    
+    static void PrimeNumber()
+    {
+        System.Console.Write("Número entero? ");
+        int inputNum = int.Parse(System.Console.ReadLine());
+        
+    }
+
+
+
+}
