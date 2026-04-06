b8ca015406e466416adcc6a9d60b03d157489f7e
Alpaca Zen <alpacazendev@gmail.com>
2026-04-06 06:10:30 -0500

    E04

---
+++ thinking-in-code/CursoCSharp2026/E04_Constantes/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace E04_Constantes
+{
+    class Program
+    {
+        public const string NOMBRE = "Pedro";
+        public const int NUMERO_MAXIMO_DE_PEDIDOS = 10;
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                if (i < NUMERO_MAXIMO_DE_PEDIDOS)
+                {
+                    int result = i;
+                    Console.WriteLine(result);
+                }
+            }
+        }
+
+
+    }
+}
