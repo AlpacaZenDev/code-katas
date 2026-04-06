945e44085894233ce306e950434b2a7fccb1987e
Alpaca Zen <alpacazendev@gmail.com>
2026-04-06 06:10:38 -0500

    E05

---
+++ thinking-in-code/CursoCSharp2026/E05_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+const bool BANDERA = false;
+
+if (BANDERA)
+{
+    // entra aquí, si bandera es true
+}
+else
+{
+    // entra aquí, si bandera es false
+}
