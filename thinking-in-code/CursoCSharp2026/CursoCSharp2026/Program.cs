7a16f91139a81dd31be9d9aa6627ef15494cb78e
Alpaca Zen <alpacazendev@gmail.com>
2026-04-06 06:10:19 -0500

    E03

---
+++ thinking-in-code/CursoCSharp2026/CursoCSharp2026/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CursoCSharp2026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
