df785193dd8918eddeca45b0eff5e3823fe9d1c8
Alpaca Zen <alpacazendev@gmail.com>
2026-04-07 06:18:47 -0500

    E08

---
+++ thinking-in-code/CursoCSharp2026/E08_while/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace E08_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            while (numero < 10)
+            {
+                Console.WriteLine(numero);
+                numero++;
+            }
+        }
+    }
+}
