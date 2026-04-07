df296f113b1797f6c536560a40145631f724c0a2
Alpaca Zen <alpacazendev@gmail.com>
2026-04-07 06:18:38 -0500

    E07

---
+++ thinking-in-code/CursoCSharp2026/E07_do_while/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace E07_do_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            do
+            {
+                Console.Write(numero);
+                numero++;
+                if (numero == 5)
+                {
+                    return;
+                }
+            }
+            while (numero < 10);
+
+        }
+    }
+}
