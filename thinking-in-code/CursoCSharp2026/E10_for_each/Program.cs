ff10c8aa1c666c3d1370f6bc5fe28a4012bcc3de
Alpaca Zen <alpacazendev@gmail.com>
2026-04-07 06:19:01 -0500

    E10

---
+++ thinking-in-code/CursoCSharp2026/E10_for_each/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E10_for_each
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hay {args.Length} argumentos!");
+        }
+    }
+}
