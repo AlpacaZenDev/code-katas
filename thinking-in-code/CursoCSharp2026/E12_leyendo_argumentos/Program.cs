e26368610982ebf31997044fddc27a37f2555781
Alpaca Zen <alpacazendev@gmail.com>
2026-04-07 06:19:10 -0500

    E12

---
+++ thinking-in-code/CursoCSharp2026/E12_leyendo_argumentos/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace E12_leyendo_argumentos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hay {args.Length} argumentos");
+
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
