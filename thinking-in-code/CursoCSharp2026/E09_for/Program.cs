6440d0b49db2f56d31fc2853c70f841d1dca989e
Alpaca Zen <alpacazendev@gmail.com>
2026-04-07 06:18:53 -0500

    E09

---
+++ thinking-in-code/CursoCSharp2026/E09_for/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace E09_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 10;
+            for (int i = 0; i < numero; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
