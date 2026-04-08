024c2a658cd7dde9438fd01b84c319a0663b4a59
Alpaca Zen <alpacazendev@gmail.com>
2026-04-08 06:04:39 -0500

    E14

---
+++ thinking-in-code/CursoCSharp2026/E14_conversiones_tipo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace E14_conversiones_tipo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double doubleValor = 8.45;
+            int enteroValor = ToInt32(doubleValor);
+
+        }
+    }
+}
