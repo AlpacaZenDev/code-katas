d266462e3d6f8bd854928cf5a1e541af62cbd6a9
Alpaca Zen <alpacazendev@gmail.com>
2026-04-08 06:04:21 -0500

    E15

---
+++ thinking-in-code/CursoCSharp2026/E15_funciones/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace E15_funciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string presentacion = Presentacion("Pedro", "López");
+        }
+
+        public static string Presentacion(string nombre, string apellido)
+        {
+            return $"Mi nombre es {nombre} {apellido}";
+        }
+
+    }
+}
