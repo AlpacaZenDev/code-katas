1e7a851a074ef59f37b649bb1efea0a156e3287a
Alpaca Zen <alpacazendev@gmail.com>
2026-04-30 05:43:16 -0500

    Interfases

---
+++ thinking-in-code/CursoCSharp2026/E01_interfaces/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E01_interfaces
+{
+    public class Car : Program, IEquatable<Car>, IComparacion
+    {
+        public bool Equals(Car obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int EsIgual(int numero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int EsIgualACoche(int numero)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ thinking-in-code/CursoCSharp2026/E01_interfaces/IEquatable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E01_interfaces
+{
+    interface IEquatable<T>
+    {
+        bool Equals(T obj);
+        int EsIgual(int numero);
+    }
+
+    public interface IComparacion
+    {
+        int EsIgualACoche(int numero);
+    }
+}
