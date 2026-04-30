3f5fc177321b81c3fee3fdf302d1502ad957031f
Alpaca Zen <alpacazendev@gmail.com>
2026-04-30 05:43:53 -0500

    Interfaces

---
+++ thinking-in-code/CursoCSharp2026/E09_interfaces/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E09_interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ thinking-in-code/CursoCSharp2026/M06E01_arraylist/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace M06E01_arraylist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
