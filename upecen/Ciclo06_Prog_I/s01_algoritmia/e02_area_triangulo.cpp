1ac1f40d7069e2e6db7b997c262efd0516a422eb
Alpaca Zen <alpacazendev@gmail.com>
2026-05-11 05:36:10 -0500

    Reordenamiento del proyecto para el inicio del ciclo 7

---
+++ upecen/Ciclo06_Prog_I/s01_algoritmia/e02_area_triangulo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Algoritmo AreaTriangulo" << endl;
+    cout << "    Definir base, altura, area Como Real" << endl;
+    cout << "    Escribir 'Ingrese la base:'" << endl;
+    cout << "    Leer base" << endl;
+    cout << "    Escribir 'Ingrese la altura:'" << endl;
+    cout << "    Leer altura" << endl;
+    cout << "    area <- (base * altura) / 2" << endl;
+    cout << "    Escribir 'El area es: ', area" << endl;
+    cout << "FinAlgoritmo" << endl;
+    return 0;
+}
