ddaa1c90fa4b97da6cde7f0a7fac76a0306b6fa7
Alpaca Zen <alpacazendev@gmail.com>
2026-06-09 18:57:51 -0500

    update

---
+++ thinking-in-code/EffectiveTS/app.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const states = [
+    { name: 'Alabama', capital: 'Montgomery' },
+    { name: 'Alaska', capital: 'Juneau' },
+    { name: 'Arizona', capital: 'Phoenix' },
+    // ...
+];
+for (const state of states) {
+    console.log(state.capital);
+}
+//# sourceMappingURL=app.js.map
