3a4e2d8e5122a1c1c73efa722186747827242a63
Alpaca Zen <alpacazendev@gmail.com>
2026-06-09 18:57:16 -0500

    cambios

---
+++ upecen/Ciclo07_Prog_II - Copy/01_EjerciciosPropuestos/E02_PositivoONegativo/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace E02_PositivoONegativo
+{
+    /**
+     * Escribe un programa que tome como entrada un número y determine si es positivo o negativo.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("#### POSITIVO O NEGATIVO ####");
+            Console.ResetColor();
+
+            Console.Write("Ingrese un número real: ");
+            var numero = ValidarNumeroReal();
+
+            string resultado = numero switch
+            {
+                < 0     => "El número ingresado es negativo",
+                > 0     => "El número ingresado es positivo",
+                _       => "El número ingresado es cero",
+            };
+            Console.WriteLine(resultado);
+        }
+
+        /// <summary>
+        /// Obliga a que el usuario ingrese un número decimal válido por consola.
+        /// No retornará hasta que la entrada sea correcta.
+        /// </summary>
+        /// <returns>Retorna el valor numérico tras lo ingresado por el usuario</returns>
+
+        static double ValidarNumeroReal()
+        {
+            double resultado;
+            while (!double.TryParse(Console.ReadLine(), out resultado))
+            {
+                Console.WriteLine("Ingrese un número real correcto: ");
+            }
+            return resultado;
+        }
+    }
+}
---
+++ upecen/Ciclo07_Prog_II - Copy/T01_EjerciciosPropuestos/E01_AreaCirculo/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace E01_AreaCirculo
+{
+    /*
+    * Escribe un programa que pida al usuario el radio de un círculo y luego 
+    * calcule y muestre el área de ese círculo. Utiliza la fórmula área = PI * radio^2. 
+    * Puedes considerar PI como 3.1416.
+    */
+    internal class Program
+    {
+        private const double PI = 3.1416;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### CALCULA EL ÁREA DEL CÍRCULO ###");
+            Console.Write("Ingrese el valor del rádio: ");
+            double radio = ValidarRadioInput();
+
+            double area = PI * Math.Pow(radio, 2);
+
+            Console.WriteLine($"\nEl área calculada es: {Math.Round(area, 2)}");
+            Console.WriteLine("Presione una tecla para salir...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Valida que el valor ingresado por el usuario sea del tipo double.
+        /// Por ahora sólo retorna '0' para valores incorrectos, aunque bien se 
+        /// puede usar un while.
+        /// </summary>
+        /// <returns>Valor del radio como double</returns>
+        /// Disclaimer: Sé bien sobre la crítica acerca que los comentarios de documentación (XML Comments)
+        /// como este, deben describir qué hace el código, no mis dudas o lo que me faltó por hacer.
+        /// Entiendo que si un programador usa mi método, no quiere saber que tuve pereza de poner 
+        /// un while; sino que quiere saber qué esperar. Sólo que esto lo hice como una excusa para
+        /// comparar entre un 'if' vs un 'while' para este contexto.
+
+        static double ValidarRadioInput()
+        {
+            if (double.TryParse(Console.ReadLine(), out double resultado))
+            {
+                return resultado;
+            }
+            Console.WriteLine("Valor ingresado es erróneo!");
+            return 0;
+        }
+    }
+}
