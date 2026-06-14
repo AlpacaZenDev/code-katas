d6c9e72686d80d672afdebdf8833e87b012714bd
Alpaca Zen <alpacazendev@gmail.com>
2026-06-14 08:53:03 -0500

    Ejercicios propuestos

--- upecen/Ciclo07_Prog_II/T01_EjerciciosPropuestos_CPP/E16_to_E20_Modular_Design/E16_ClaseConversion/Conversion.cpp
+++
@@ -1,6 +0,0 @@
-#include "Conversion.h"
-
-Conversion::Conversion(double m) : metros(m) {}
-
-double Conversion::aCentimetros() { return metros * 100.0; }
-double Conversion::aMilimetros() { return metros * 1000.0; }
